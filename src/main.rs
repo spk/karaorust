96f343e121e3145c8318add4c262030705c43c4a
Laurent Arnoud <laurent@spkdev.net>
2015-08-10 23:37:25 +0200

    Experiment with combine crates

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,101 @@
-use std::io::{BufReader, BufRead};
-use std::fs::File;
-use std::path::{Path};
-use std::thread::sleep_ms;
-use std::env;
+extern crate combine;
+use combine::*;
+use combine::primitives::Stream;
+
+#[derive(PartialEq, Debug)]
+pub struct Header {
+    key: String,
+    value: String,
+}
+
+#[derive(PartialEq, Debug)]
+pub struct Lyric {
+    duration: u32,
+    text: String,
+}
+
+#[derive(PartialEq, Debug)]
+pub struct Karaoke {
+    pub headers: Vec<Header>,
+    pub lyrics: Vec<Lyric>
+}
+
+fn header<I>(input: State<I>) -> ParseResult<Header, I>
+where I: Stream<Item=char> {
+    let lex_char = |c| char(c).skip(spaces());
+    (lex_char('#')
+     , many::<String, _>(alpha_num())
+     , lex_char(':')
+     , many::<String, _>(satisfy(|c| c != '\n')))
+        .map(|(_, key, _, value)| {
+            Header {
+                key: key,
+                value: value.trim_right().to_string()
+            }
+        }).parse_state(input)
+}
+
+fn lyric<I>(input: State<I>) -> ParseResult<Lyric, I>
+where I: Stream<Item=char> {
+    (char(':')
+     , spaces()
+     , many::<String, _>(digit())
+     , spaces()
+     , many::<String, _>(digit())
+     , spaces()
+     , many::<String, _>(digit())
+     , spaces()
+     , many::<String, _>(satisfy(|c| c != '\n')))
+        .map(|(_, _, _, _, duration, _, _, _, text)| {
+            Lyric {
+                duration: duration.parse::<u32>().unwrap() * 100,
+                text: text.trim_right().to_string()
+            }
+        }).parse_state(input)
+}
+
+fn karaoke<I>(input: State<I>) -> ParseResult<Karaoke, I>
+where I: Stream<Item=char> {
+    (parser(header), newline(), parser(lyric), newline(), char('E'))
+        .map(|(h, _, l, _, _)| {
+            Karaoke {
+                headers: vec![h],
+                lyrics: vec![l]
+            }
+    }).parse_state(input)
+}
 
 #[cfg(not(test))]
 fn main() {
-    if let Some(input) = env::args().nth(1) {
-        let file = match File::open(&Path::new(&input)) {
-            Ok(file) => file,
-            Err(..)  => panic!("bim"),
-        };
-        let reader = BufReader::new(&file);
-        let lines: Vec<String> = reader.lines().map(|x| x.unwrap()).collect();
+}
 
-        for line in lines.iter() {
-            if line.starts_with('#') {
-                let metadata: Vec<&str> = line.split(":").collect();
-                println!("{} ~> {}", metadata[0], metadata[1]);
-            } else if line.starts_with(':') {
-                let row: Vec<&str> = line.split(' ').collect();
-                let duration = row[2].parse::<u32>().unwrap() * 100;
-                let text = &row[4 .. row.len()].connect(" ");;
-                println!("{}", text);
-                sleep_ms(duration);
-            } else if line.starts_with('-') {
-                println!("{}", "");
-            }
-        }
-    }
+#[test]
+fn test_header_line() {
+    let lines = "#TITLE:Natalie's Rap";
+    let result = parser(header).parse(lines);
+    let expr = Header { key: "TITLE".to_string(),
+                        value: "Natalie's Rap".to_string()
+    };
+    assert_eq!(result, Ok((expr, "")));
+}
+
+#[test]
+fn test_lyric_line() {
+    let lines = ": 1 13 50 We're sitting here today with";
+    let result = parser(lyric).parse(lines);
+    let expr = Lyric { duration: 1300,
+                       text: "We\'re sitting here today with".to_string()
+    };
+    assert_eq!(result, Ok((expr, "")));
+}
+
+#[test]
+fn test_karaoke_simple() {
+    let lines = "#TITLE:Natalie's Rap\n: 1 13 50 We're sitting here today with\nE";
+    let result = parser(karaoke).parse(lines);
+    let expr = Karaoke {
+        headers: vec![Header { key: "TITLE".to_string(), value: "Natalie's Rap".to_string()}],
+        lyrics: vec![Lyric { duration: 1300, text: "We\'re sitting here today with".to_string() }]
+    };
+    assert_eq!(result, Ok((expr, "")));
 }
