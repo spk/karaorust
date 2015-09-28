5357d3b9be7a6fc6f5b2c848fff905b629960088
Laurent Arnoud <laurent@spkdev.net>
2015-09-28 21:11:57 +0200

    Better error handling on args and add usage

--- src/main.rs
+++ src/main.rs
@@ -5,28 +5,53 @@ use std::thread::sleep_ms;
 use std::env;
 
 #[cfg(not(test))]
-fn main() {
-    if let Some(input) = env::args().nth(1) {
-        let file = match File::open(&Path::new(&input)) {
-            Ok(file) => file,
-            Err(..)  => panic!("bim"),
-        };
-        let reader = BufReader::new(&file);
-        let lines: Vec<String> = reader.lines().map(|x| x.unwrap()).collect();
+fn print_usage(program: &str) {
+    let brief = format!("Usage: {} FILE", program);
+    print!("{}", &brief);
+}
 
-        for line in lines.iter() {
-            if line.starts_with('#') {
-                let metadata: Vec<&str> = line.split(":").collect();
-                println!("{} ~> {}", metadata[0], metadata[1]);
-            } else if line.starts_with(':') {
-                let row: Vec<&str> = line.split(' ').collect();
-                let duration = row[2].parse::<u32>().unwrap() * 100;
-                let text = &row[4 .. row.len()].join(" ");;
-                println!("{}", text);
-                sleep_ms(duration);
-            } else if line.starts_with('-') {
-                println!("{}", "");
-            }
+#[cfg(not(test))]
+fn check_argv(mut argv: env::Args) -> Result<String, String> {
+    argv.nth(1)
+        .ok_or("Please give at least one argument".to_owned())
+}
+
+#[cfg(not(test))]
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let program = args[0].clone();
+    let input = match check_argv(env::args()) {
+        Ok(n) => n,
+        Err(err) => {
+            println!("Error: {}", err);
+            print_usage(&program);
+            return;
+        },
+    };
+
+    let file = match File::open(&Path::new(&input)) {
+        Ok(file) => file,
+        Err(err)  => {
+            println!("Error: {}", err);
+            print_usage(&program);
+            return;
+        },
+    };
+    let reader = BufReader::new(&file);
+    let lines: Vec<String> = reader.lines().map(|x| x.unwrap()).collect();
+
+    for line in lines.iter() {
+        if line.starts_with('#') {
+            let metadata: Vec<&str> = line.split(":").collect();
+            println!("{} ~> {}", metadata[0], metadata[1]);
+        } else if line.starts_with(':') {
+            let row: Vec<&str> = line.split(' ').collect();
+            let duration = row[2].parse::<u32>().unwrap() * 100;
+            let text = &row[4 .. row.len()].join(" ");;
+            println!("{}", text);
+            sleep_ms(duration);
+        } else if line.starts_with('-') {
+            println!("{}", "");
         }
     }
 }
