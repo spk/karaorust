952ee58fd84c5005116f9cbdb8a8742d9df83f6d
Laurent Arnoud <laurent@spkdev.net>
2015-10-15 20:04:45 +0200

    Use real world karaoke file and fix tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,40 +1,40 @@
 #![crate_name = "karaorust"]
 
+use std::collections::HashMap;
+
 extern crate combine;
 use combine::*;
 use combine::primitives::Stream;
 
 #[derive(PartialEq, Debug)]
-pub struct Header {
-    key: String,
-    value: String,
-}
-
-#[derive(PartialEq, Debug)]
 pub struct Lyric {
-    duration: u32,
+    duration: i32,
     text: String,
 }
 
 #[derive(PartialEq, Debug)]
 pub struct Karaoke {
-    pub headers: Vec<Header>,
+    pub header: HashMap<String, String>,
     pub lyrics: Vec<Lyric>
 }
 
-pub fn header<I>(input: State<I>) -> ParseResult<Header, I>
+pub fn header<I>(input: State<I>) -> ParseResult<(String, String), I>
 where I: Stream<Item=char> {
     let lex_char = |c| char(c).skip(spaces());
     (lex_char('#')
      , many::<String, _>(alpha_num())
      , lex_char(':')
-     , many::<String, _>(satisfy(|c| c != '\n')))
-        .map(|(_, key, _, value)| {
-            Header {
-                key: key,
-                value: value.trim_right().to_string()
-            }
-        }).parse_state(input)
+     , many::<String, _>(satisfy(|c| c != '\n'))
+     , newline())
+        .map(|(_, key, _, value, _)| (key, value.trim_right().to_string()))
+        .expected("header")
+        .parse_state(input)
+}
+
+fn headers<I>(input: State<I>) -> ParseResult<HashMap<String, String>, I>
+where I: Stream<Item=char> {
+    many(parser(header))
+        .parse_state(input)
 }
 
 pub fn lyric<I>(input: State<I>) -> ParseResult<Lyric, I>
@@ -47,22 +47,32 @@ where I: Stream<Item=char> {
      , spaces()
      , many::<String, _>(digit())
      , spaces()
-     , many::<String, _>(satisfy(|c| c != '\n')))
-        .map(|(_, _, _, _, duration, _, _, _, text)| {
+     , many::<String, _>(satisfy(|c| c != '\n'))
+     , newline())
+        .map(|(_, _, _, _, duration, _, _, _, text, _)| {
             Lyric {
-                duration: duration.parse::<u32>().unwrap() * 100,
+                duration: duration.parse::<i32>().unwrap() * 100,
                 text: text.trim_right().to_string()
             }
-        }).parse_state(input)
+        })
+    .expected("lyric")
+    .parse_state(input)
+}
+
+fn split<I>(input: State<I>) -> ParseResult<(), I>
+where I: Stream<Item=char> {
+    let split = (token('-'), skip_many(satisfy(|c| c != '\n'))).map(|_| ());
+    skip_many(skip_many1(space()).or(split))
+        .parse_state(input)
 }
 
 pub fn karaoke<I>(input: State<I>) -> ParseResult<Karaoke, I>
 where I: Stream<Item=char> {
-    (parser(header), newline(), parser(lyric), newline(), char('E'))
-        .map(|(h, _, l, _, _)| {
+    (parser(headers), many(parser(lyric).skip(parser(split))), parser(split), char('E'))
+        .map(|(h, l, _, _)| {
             Karaoke {
-                headers: vec![h],
-                lyrics: vec![l]
+                header: h,
+                lyrics: l
             }
     }).parse_state(input)
 }
--- src/tests.rs
+++ src/tests.rs
@@ -1,33 +1,64 @@
 use super::*;
+
+use std::collections::HashMap;
 use combine::*;
+
+use std::io::prelude::*;
+use std::fs::File;
+use std::path::Path;
 
 #[test]
 fn test_header_line() {
-    let lines = "#TITLE:Natalie's Rap";
-    let result = parser(header).parse(lines);
-    let expr = Header { key: "TITLE".to_string(),
-                        value: "Natalie's Rap".to_string()
+    let lines = "#TITLE:Natalie's Rap\n#ARTIST:Natalie Portman\nE";
+    let result = parser(karaoke).parse(lines);
+    let mut header = HashMap::new();
+    header.insert("TITLE".to_string(), "Natalie\'s Rap".to_string());
+    header.insert("ARTIST".to_string(), "Natalie Portman".to_string());
+    let expected = Karaoke {
+        header: header,
+        lyrics: vec![]
     };
-    assert_eq!(result, Ok((expr, "")));
-}
-
-#[test]
-fn test_lyric_line() {
-    let lines = ": 1 13 50 We're sitting here today with";
-    let result = parser(lyric).parse(lines);
-    let expr = Lyric { duration: 1300,
-                       text: "We\'re sitting here today with".to_string()
-    };
-    assert_eq!(result, Ok((expr, "")));
+    match result {
+        Ok(result) => assert_eq!(result, (expected, "")),
+        Err(err) => {
+            println!("{}", err);
+            assert!(false);
+        }
+    }
 }
 
 #[test]
 fn test_karaoke_simple() {
-    let lines = "#TITLE:Natalie's Rap\n: 1 13 50 We're sitting here today with\nE";
+    let lines = "#TITLE:Natalie's Rap\n: 1 13 50 We're sitting here today with\n- 13\nE";
     let result = parser(karaoke).parse(lines);
-    let expr = Karaoke {
-        headers: vec![Header { key: "TITLE".to_string(), value: "Natalie's Rap".to_string()}],
+    let mut header = HashMap::new();
+    header.insert("TITLE".to_string(), "Natalie\'s Rap".to_string());
+    let expected = Karaoke {
+        header: header,
         lyrics: vec![Lyric { duration: 1300, text: "We\'re sitting here today with".to_string() }]
     };
-    assert_eq!(result, Ok((expr, "")));
+    match result {
+        Ok(result) => assert_eq!(result, (expected, "")),
+        Err(err) => {
+            println!("{}", err);
+            assert!(false);
+        }
+    }
+}
+
+#[test]
+fn test_karaoke_complete() {
+    let mut data = String::new();
+    File::open(&Path::new(&"data/Natalie_Portman_-_Natalies_Rap.txt"))
+        .and_then(|mut file| file.read_to_string(&mut data))
+        .unwrap();
+    let text = from_iter(data.chars());
+    match parser(karaoke).parse(text.clone()) {
+        Ok((Karaoke(_), _)) => (),
+        Ok(_) => assert!(false),
+        Err(err) => {
+            println!("{}", err);
+            assert!(false);
+        }
+    }
 }
