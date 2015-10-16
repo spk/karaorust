4af34123665ecfbc5238dba9eabd374fd0fd4e61
Laurent Arnoud <laurent@spkdev.net>
2015-10-16 20:16:25 +0200

    Replace dirty bin by karaoke parser

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,7 @@ use std::fs::File;
 use std::path::Path;
 use std::io::prelude::*;
 use std::process;
+use std::env;
 
 extern crate combine;
 use combine::*;
@@ -13,8 +14,8 @@ use combine::primitives::Stream;
 
 #[derive(PartialEq, Debug)]
 pub struct Lyric {
-    duration: i32,
-    text: String,
+    pub duration: i32,
+    pub text: String,
 }
 
 #[derive(PartialEq, Debug)]
@@ -82,9 +83,26 @@ where I: Stream<Item=char> {
     }).parse_state(input)
 }
 
-fn print_usage(program: &str) {
+pub fn print_usage(program: &str) {
     let brief = format!("Usage: {} FILE", program);
-    print!("{}", &brief);
+    println!("{}", &brief);
+}
+
+fn check_argv(mut argv: env::Args) -> Result<String, String> {
+    argv.nth(1)
+        .ok_or("Please give at least one argument".to_owned())
+}
+
+pub fn input_from_args(args: env::Args, program: &str) -> String {
+    let input = match check_argv(args) {
+        Ok(n) => n,
+        Err(err) => {
+            println!("Error: {}", err);
+            print_usage(&program);
+            process::exit(1);
+        },
+    };
+    input
 }
 
 pub fn read_karaoke_file(input: &str, program: &str) -> String {
@@ -92,7 +110,7 @@ pub fn read_karaoke_file(input: &str, program: &str) -> String {
     let display = path.display();
     let mut file = match File::open(&path) {
         Err(err) => {
-            println!("couldn't open {}: {}", display, Error::description(&err));
+            println!("Couldn't open {}: {}", display, Error::description(&err));
             print_usage(&program);
             process::exit(1);
         },
@@ -102,11 +120,11 @@ pub fn read_karaoke_file(input: &str, program: &str) -> String {
     let mut buffer = String::new();
     match file.read_to_string(&mut buffer) {
         Err(err) => {
-            println!("couldn't read {}: {}", display, Error::description(&err));
+            println!("Couldn't read {}: {}", display, Error::description(&err));
             print_usage(&program);
             process::exit(1);
         },
-        Ok(_) => print!("Starting karaoke with {}", display),
+        Ok(_) => println!("Starting karaoke with {}", display),
     }
     buffer
 }
--- src/main.rs
+++ src/main.rs
@@ -1,57 +1,35 @@
-use std::io::{BufReader, BufRead};
-use std::fs::File;
-use std::path::{Path};
+extern crate karaorust;
+extern crate combine;
+
 use std::thread::sleep_ms;
 use std::env;
 
-#[cfg(not(test))]
-fn print_usage(program: &str) {
-    let brief = format!("Usage: {} FILE", program);
-    print!("{}", &brief);
-}
-
-#[cfg(not(test))]
-fn check_argv(mut argv: env::Args) -> Result<String, String> {
-    argv.nth(1)
-        .ok_or("Please give at least one argument".to_owned())
-}
+use karaorust::*;
+use combine::*;
 
 #[cfg(not(test))]
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let program = args[0].clone();
-    let input = match check_argv(env::args()) {
-        Ok(n) => n,
+    let args = env::args();
+    let args_vec: Vec<String> = args.collect();
+    let program = args_vec[0].clone();
+
+    let input = input_from_args(env::args(), &program);
+    let buffer = read_karaoke_file(&input, &program);
+    let text = from_iter(buffer.chars());
+
+    match parser(karaoke).parse(text.clone()) {
+        Ok((k, _)) => {
+            for (k, v) in k.header.iter() {
+                println!("{} ~> {}", k, v);
+            }
+            for lyric in k.lyrics.iter() {
+                println!("{}", lyric.text);
+                sleep_ms(lyric.duration as u32);
+            }
+        },
         Err(err) => {
-            println!("Error: {}", err);
-            print_usage(&program);
+            println!("{}", err);
             return;
-        },
-    };
-
-    let file = match File::open(&Path::new(&input)) {
-        Ok(file) => file,
-        Err(err)  => {
-            println!("Error: {}", err);
-            print_usage(&program);
-            return;
-        },
-    };
-    let reader = BufReader::new(&file);
-    let lines: Vec<String> = reader.lines().map(|x| x.unwrap()).collect();
-
-    for line in lines.iter() {
-        if line.starts_with('#') {
-            let metadata: Vec<&str> = line.split(":").collect();
-            println!("{} ~> {}", metadata[0], metadata[1]);
-        } else if line.starts_with(':') {
-            let row: Vec<&str> = line.split(' ').collect();
-            let duration = row[2].parse::<u32>().unwrap() * 100;
-            let text = &row[4 .. row.len()].join(" ");;
-            println!("{}", text);
-            sleep_ms(duration);
-        } else if line.starts_with('-') {
-            println!("{}", "");
         }
-    }
+    };
 }
